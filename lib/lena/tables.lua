c3f33cef03e24dd97d1e33399b9f5a69c2835632
Lena <lena.love2000@web.de>
2023-07-22 15:24:02 +0200

    Create tables.lua

---
+++ lib/lena/tables.lua
@@ -0,0 +1,144 @@
+
+flare_veh = {788747387, -82626025, 1181327175, -1281684762}
+anti_explo_sniper = {"Remove Weapon", "Remove Component", "Notify", "Kill", "Kick"}
+interior_stuff = {0, 233985, 169473, 169729, 169985, 170241, 177665, 177409, 185089, 184833, 184577, 163585, 167425, 167169}
+launch_vehicle = {"Launch Up", "Launch Forward", "Launch Backwards", "Launch Down", "Slingshot"}
+better_heli_offsets = {0x18, 0x20, 0x24, 0x30, 0x48, 0x4C, 0x58, 0x3C}
+better_planes = {
+    {0x00B0, 10},
+    {0x0050, 0.3},
+    {0x0054, 1}
+}
+
+MCLabels = {
+    "CLBHBKRREG",
+    "ARC_HT_1",
+    "ARC_HT_1B",
+}
+
+CEOLabels = {
+    "HIP_HELP_BBOSS",
+    "HIP_HELP_BBOSS2",
+    "HPBOARD_REG",
+    "HPBOARD_REGB",
+    "HT_NOT_BOSS",
+    "HUB_PC_BLCK",
+    "NHPG_HELP_BBOSS",
+    "OFF_COMP_REG",
+    "TRUCK_PC_BLCK",
+    "TUN_HELP_BBOSS",
+    "BUNK_PC_BLCK",
+    "CH_FINALE_REG",
+    "CH_PREP_REG",
+    "CH_SETUP_REG",
+    "FHQ_PC_BLCK",
+    "HANG_PC_BLCK",
+    "HFBOARD_REG",
+    "HIBOARD_REG",
+    "HIBOARD_REGB",
+    "MP_OFF_LAP_1",
+    "MP_OFF_LAP_PC",
+    "OFF_COMP_REG",
+    "ARC_PC_BLCK",
+    "ARC_HT_0",
+    "ARC_HT_0B",
+    "ACID_SLL_HLP2",
+    "HRBOARD_REG",
+    "HRBOARD_REGB",
+    "FHQ_SEAT_RGSIT1"
+}
+
+script_start = {
+    {"appsmuggler", "Open Air Cargo Screen"},
+    {"appbusinesshub", "Open Nightclub Screen"},
+    {"apparcadebusinesshub", "Open Master Control Terminal"}
+}
+
+veh_things = {
+    "brickade2",
+    "hauler",
+    "hauler2",
+    "manchez3",
+    "terbyte",
+    "minitank",
+    "rcbandito",
+    "volatus",
+    "supervolito",
+    "supervolito2"
+}
+
+numpadControls = {
+    -- Plane & Heli
+    107,
+    108,
+    109,
+    110,
+    111,
+    112,
+    117,
+    118,
+    -- Submarine
+    123,
+    124,
+    125,
+    126,
+    127,
+    128,
+}
+
+interiors = {
+    {"AFK Room", {x=-158.71494, y=-982.75885, z=149.13135}},
+    {"Torture Room", {x=147.170, y=-2201.804, z=4.688}},
+    {"Mining Tunnels", {x=-595.48505, y=2086.4502, z=131.38136}},
+    {"Omegas Garage", {x=2330.2573, y=2572.3005, z=46.679367}},
+    {"50 Car Garage", {x=520.0, y=-2625.0, z=-50.0}},
+    {"Server Farm", {x=2474.0847, y=-332.58887, z=92.9927}},
+    {"Character Creation", {x=402.91586, y=-998.5701, z=-99.004074}},
+    {"Life Invader Building", {x=-1082.8595, y=-254.774, z=37.763317}},
+    {"Mission End Garage", {x=405.9228, y=-954.1149, z=-99.6627}},
+    {"Destroyed Hospital", {x=304.03894, y=-590.3037, z=43.291893}},
+    {"Stadium", {x=-256.92334, y=-2024.9717, z=30.145584}},
+    {"Comedy Club", {x=-430.00974, y=261.3437, z=83.00648}},
+    {"Record A Studios", {x=-1010.6883, y=-49.127754, z=-99.40313}},
+    {"Bahama Mamas Nightclub", {x=-1394.8816, y=-599.7526, z=30.319544}},
+    {"Janitors House", {x=-110.20285, y=-8.6156025, z=70.51957}},
+    {"Therapists House", {x=-1913.8342, y=-574.5799, z=11.435149}},
+    {"Martin Madrazos House", {x=1395.2512, y=1141.6833, z=114.63437}},
+    {"Floyds Apartment", {x=-1156.5099, y=-1519.0894, z=10.632717}},
+    {"Michaels House", {x=-813.8814, y=179.07889, z=72.15914}},
+    {"Franklins House (Strawberry)", {x=-14.239959, y=-1439.6913, z=31.101551}},
+    {"Franklins House (Vinewood Hills)", {x=7.3125067, y=537.3615, z=176.02803}},
+    {"Trevors House", {x=1974.1617, y=3819.032, z=33.436287}},
+    {"Lesters House", {x=1273.898, y=-1719.304, z=54.771}},
+    {"Lesters Warehouse", {x=713.5684, y=-963.64795, z=30.39534}},
+    {"Lesters Office", {x=707.2138, y=-965.5549, z=30.412853}},
+    {"Meth Lab", {x=1391.773, y=3608.716, z=38.942}},
+    {"Acid Lab", {x=484.69, y=-2625.36, z=-49.0}},
+    {"Morgue Lab", {x=495.0, y=-2560.0, z=-50.0}},
+    {"Humane Labs", {x=3625.743, y=3743.653, z=28.69009}},
+    {"Motel Room", {x=152.2605, y=-1004.471, z=-99.024}},
+    {"Police Station", {x=443.4068, y=-983.256, z=30.689589}},
+    {"Bank Vault", {x=263.39627, y=214.39891, z=101.68336}},
+    {"Blaine County Bank", {x=-109.77874, y=6464.8945, z=31.626724}},
+    {"Tequi-La-La Bar", {x=-564.4645, y=275.5777, z=83.074585}},
+    {"Scrapyard Body Shop", {x=485.46396, y=-1315.0614, z=29.2141}},
+    {"The Lost MC Clubhouse", {x=980.8098, y=-101.96038, z=74.84504}},
+    {"Vangelico Jewlery Store", {x=-629.9367, y=-236.41296, z=38.057056}},
+    {"Airport Lounge", {x=-913.8656, y=-2527.106, z=36.331566}},
+    {"Morgue", {x=240.94368, y=-1379.0645, z=33.74177}},
+    {"Union Depository", {x=1.298771, y=-700.96967, z=16.131021}},
+    {"Fort Zancudo Tower", {x=-2357.9187, y=3249.689, z=101.45073}},
+    {"Agency Interior", {x=-1118.0181, y=-77.93254, z=-98.99977}},
+    {"Agency Garage", {x=-1071.0494, y=-71.898506, z=-94.59982}},
+    {"Terrobyte Interior", {x=-1421.015, y=-3012.587, z=-80.000}},
+    {"Bunker Interior", {x=899.5518,y=-3246.038, z=-98.04907}},
+    {"IAA Office", {x=128.20, y=-617.39, z=206.04}},
+    {"FIB Top Floor", {x=135.94359, y=-749.4102, z=258.152}},
+    {"FIB Floor 47", {x=134.5835, y=-766.486, z=234.152}},
+    {"FIB Floor 49", {x=134.635, y=-765.831, z=242.152}},
+    {"Strip Club DJ Booth", {x=121.398254, y=-1281.0024, z=29.480522}},
+    {"Single Garage", {x=-144.11609, y=-576.5855, z=31.845743}},
+    {"FIB Destroyed Floor", {x=158.44386, y=-738.07367, z=246.15218}},
+    {"Pharma Lab", {x=496.86224, y=-2560.0608, z=-58.921993}},
+    {"Nightclub Safe", {x=-1615.6887, y=-3015.7354, z=-75.205086}}
+}
