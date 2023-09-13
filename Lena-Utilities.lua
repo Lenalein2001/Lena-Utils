8574fa83b7f6886f06ae5bb9dd069e27035c586f
Lena <lena.love2000@web.de>
2023-09-13 20:39:33 +0200

    Update Lena-Utilities.lua

--- Lena-Utilities.lua
+++ Lena-Utilities.lua
@@ -2790,7 +2790,7 @@ if is_developer() then
 
         menu.action(nativehud, "Get Warning Screen hash", {""}, "", function()
             local hash = HUD.GET_WARNING_SCREEN_MESSAGE_HASH()
-            if hash != (nil or 0) then
+            if hash then
                 log($"[Lena | Debug] Warning Screen hash: {hash}.")
             end
         end)
@@ -2827,7 +2827,7 @@ if is_developer() then
             ENTITY.FREEZE_ENTITY_POSITION(whore, true)
             -- TASK.TASK_START_SCENARIO_IN_PLACE(whore, "WORLD_HUMAN_PROSTITUTE_HIGH_CLASS", 0, false) -- Shrugs
         end)
-    --end
+--  end
 end
 
 --------------------------------------------------------------------------------
@@ -2858,7 +2858,7 @@ local function player(pid)
         0x0C080BB7, 0x02946AEA, 0x009DC11A, 0x0D539ECC, 0x0652306A, 0x03EF8419, 0x01C71674, 0x084EBAB3, 0x0BFDD257, 0x02F82A67, 0x0D4B35D2, 0x0D2F87B9, 0x09549E51, 0x0D629E9C,
         0x0AF3A2B8, 0x080BF2F7, 0x0A5DA9FC, 0x099E825A, 0x0B161719, 0x06FF828E, 0x02E5C6D7, 0x0BF98D84, 0x0DABD8F8, 0x0DAEDE69, 0x09E14D15, 0x0DB45F9C, 0x09BFE973, 0x09B1BBC0,
         0x0D64813B, 0x09F8116F, 0x0CE57ABC, 0x0D153AD5, 0x0AC5F5CA, 0x0C10591C, 0x05B1086B, 0x07F5705B, 0x085006CF, 0x0003FB87, 0x0D2341D4, 0x0B7C2834, 0x0DE9BC44, 0x07FB143B,
-        0x0A14CDAF, 0x0C1FF830, 0x0DFA57F9, 0x0C899654, 
+        0x0A14CDAF, 0x0C1FF830, 0x0DFA57F9, 0x0C899654, 0x0B8B1D52, 
         -- Retard/Sexual Abuser
         0x0CE7F2D8, 0x0CDF893D, 0x0C50A424, 0x0C68262A, 0x0CEA2329, 0x0D040837, 0x0A0A1032, 0x0D069832, 0x0B7CF320
     }
@@ -3513,14 +3513,12 @@ local function player(pid)
             local cords = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(player, -5.0, 0.0, 0.0)
             ENTITY.SET_ENTITY_COORDS(clone, cords)
             entities.set_can_migrate(clone, false)
-            --TASK.TASK_COMBAT_PED(clone, player, 0, 16)
-            --WEAPON.GIVE_WEAPON_TO_PED(clone, joaat("WEAPON_HOMINGLAUNCHER"), 20, false, true)
         end)
 
         menu.toggle_loop(trolling, "Transaction Error", {""}, "", function()
             for players.list(false, false, true) as p do
                 if not players.is_in_interior(p) and not players.is_godmode(p) and players.get_bounty(p) == nil then
-                    trigger_commands($"bounty {players.get_name(p)} 10000")
+                    trigger_commands($"bounty {players.get_name(p)} 5000")
                 end
                 if players.get_bounty(p) != nil and PLAYER.IS_PLAYER_PLAYING(p) then
                     trigger_commands($"as {players.get_name(pid)} explode {players.get_name(p)}")
