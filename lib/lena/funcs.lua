6fea044efdcc4a305cb81a9275b2a1b8ba3e0567
Lena A <lena.love2000@web.de>
2023-10-27 19:42:28 +0200

    Added get_stand_model(model)
    Added a function to get Stand's Models in `Self>Appearance>Transform`

--- lib/lena/funcs.lua
+++ lib/lena/funcs.lua
@@ -194,6 +194,61 @@ function trapcage(pid, object, visible = true)
     STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(hash)
 end
 
+function get_stand_model(model)
+    local translated = model
+    local translations = {
+        player_zero = 'Michael',
+        player_one = 'Franklin',
+        player_two = 'Trevor',
+        mp_f_freemode_01 = 'Online Female',
+        mp_m_freemode_01 = 'Online Male',
+        cs_orleans = 'Bigfoot',
+        ig_orleans = 'Bigfoot 2',
+        a_c_boar = 'Boar',
+        a_c_boar_02 = 'Boar 2',
+        a_c_cat_01 = 'Cat',
+        a_c_chimp = 'Chimp',
+        a_c_chimp_02 = 'Chimp 2',
+        a_c_chop = 'Chop',
+        a_c_chop_02 = 'Chop 2',
+        a_c_cow = 'Cow',
+        a_c_coyote = 'Coyote',
+        a_c_coyote_02 = 'Coyote 2',
+        a_c_deer = 'Deer',
+        a_c_deer_02 = 'Deer 2',
+        a_c_shepherd = 'German Shepherd',
+        a_c_hen = 'Hen',
+        a_c_husky = 'Husky',
+        a_c_mtlion = 'Mountain Lion',
+        a_c_mtlion_02 = 'Mountain Lion 2',
+        a_c_panther = 'Panther',
+        a_c_pig = 'Pig',
+        a_c_poodle = 'Poodle',
+        a_c_pug = 'Pug',
+        a_c_pug_02 = 'Pug 2',
+        a_c_rabbit = 'Rabbit',
+        a_c_rabbit_02 = 'Rabbit 2',
+        a_c_rat = 'Rat',
+        a_c_retriever = 'Golden Retriever',
+        a_c_rhesus = 'Rhesus',
+        a_c_rottweiler = 'Rottweiler',
+        a_c_westy = 'Westy',
+        a_c_dolphin = 'Dolphin',
+        a_c_fish = 'Fish',
+        a_c_sharkhammer = 'Hammer Shark',
+        a_c_humpback = 'Humpback',
+        a_c_killerwhale = 'Killer Whale',
+        a_c_stingray = 'Stingray',
+        a_c_sharktiger = 'Tiger Shark',
+        a_c_cormorant = 'Cormorant',
+        a_c_chickenhawk = 'Chicken Hawk',
+        a_c_crow = 'Crow', 
+        a_c_pigeon = 'Pigeon',
+        a_c_seagull = 'Seagull'
+    } 
+    return translations[model]
+end
+
 function BitTest(bits, place)
     return (bits & (1 << place)) ~= 0
 end
